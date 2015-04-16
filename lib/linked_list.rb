96c7f43e35bc4513bbf6d591a8aac22a62b31b49
mollytaryn <mollytaryn@gmail.com>
2015-04-16 10:20:36 -0500

    ll tests are passing, on 9b of linked list tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,48 @@
-class LinkedList
+require_relative 'linked_list_item'
 
+class LinkedList
+  attr_reader :payload
+  attr_accessor :size
+
+  def initialize
+    @size = 0
+  end
+
+  def last
+    if @size == 0
+      @size = nil
+    else
+      return @last_item.payload
+    end
+  end
+
+  def to_s
+    "|#{@first_item} |"
+  end
+
+  def push(item)
+    if @first_item.nil?
+      @first_item = LinkedListItem.new(item)
+      @last_item = @first_item
+    else
+      @last_item.next_item = LinkedListItem.new(item)
+      @last_item = @last_item.next_item
+    end
+    @size += 1
+  end
+
+  def get(index)
+    raise IndexError if index < 0 || @first_item == nil || @size < index
+    if index == 0
+      @first_item.payload
+    else
+      count = 0
+      current_node = @first_item
+      while count < index
+        current_node = current_node.next_item
+        count = count + 1
+      end
+      current_node.payload
+    end
+  end
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,8 +1,37 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload
+  attr_reader :payload, :next_item, :last
 
-  def initialize(data)
-    @payload = data
+  def initialize(payload)
+    @payload = payload
   end
+
+  def ===(another)
+    self.equal? another
+  end
+
+  def <=>(another_payload)
+
+    if self.payload.class == Symbol && another_payload.payload.class == String
+      return 1
+    elsif self.payload.class == String && another_payload.payload.class == Symbol
+      return -1
+    elsif self.payload.class == Fixnum && another_payload.payload.class == String
+      return -1
+    elsif self.payload.class == String && another_payload.payload.class == Fixnum
+      return 1
+    else
+    self.payload <=> another_payload.payload
+    end
+  end
+
+  def next_item=(lli)
+    raise ArgumentError, "Can't set next_item to self" if lli == self
+    @next_item = lli
+  end
+
+  def last?
+    next_item.nil?
+  end
+
 end
