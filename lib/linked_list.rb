ddbe3205d126075a683ef26df02b6fcf3d8aeb8c
mollytaryn <mollytaryn@gmail.com>
2015-04-17 16:00:58 -0500

    through 13F

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,34 +1,45 @@
 require_relative 'linked_list_item'
 
 class LinkedList
-  attr_reader :payload
-  attr_accessor :size
+  attr_reader :size
 
-  def initialize
+  def initialize(*payload)
     @size = 0
+    payload.each {|payload| push(payload)}
   end
 
   def last
-    if @size == 0
-      @size = nil
-    else
-      return @last_item.payload
+    unless @last_item.nil?
+      @last_item.payload
     end
   end
 
+  # def to_s
+  #   ll = ""
+  #   if @size == 0
+  #     "| |"
+  #   elsif @size == 1
+  #     "| #{@first_item.payload} |"
+  #   else
+  #     (@size - 1).times do |i|
+  #       ll << "#{get(i)}, "
+  #     end
+  #       ll << "#{@last_item.payload} "
+  #     return "| #{ll}|"
+  #   end
+  # end
+
   def to_s
-    ll = ""
-    if @size == 0
-      "| |"
-    elsif @size == 1
-      "| #{@first_item.payload} |"
-    else
-      (@size - 1).times do |i|
-        ll << "#{get(i)}, "
-      end
-        ll << "#{@last_item.payload} "
-      return "| #{ll}|"
+    str = "| "
+    current_item = @first_item
+    until current_item.nil?
+      str << current_item.payload
+      punctuation = current_item.last? ? " " : ", "
+      str << punctuation
+      current_item = current_item.next_item
     end
+    str << "|"
+    str
   end
 
   def push(item)
@@ -42,16 +53,43 @@ class LinkedList
     @size += 1
   end
 
+  def [](index)
+    get(index)
+  end
+
+  def []=(index, new_item)
+    current_node = @first_item
+    index.times do
+      current_node = current_node.next_item
+    end
+    current_node.payload = new_item
+  end
+
   def get(index)
-    raise IndexError if index < 0 || @first_item == nil || @size < index
+    raise IndexError if index < 0 || @size < index
     if index == 0
       @first_item.payload
     else
       current_node = @first_item
       index.times do
         current_node = current_node.next_item
+        raise IndexError if current_node.nil?
       end
       current_node.payload
     end
   end
+
+  def delete(index)
+    current_node = @first_item
+    if index > 0
+      (index - 1).times do
+        current_node = current_node.next_item
+      end
+      current_node.next_item = current_node.next_item.next_item
+    elsif index <=0
+      @first_item = current_node.next_item
+    end
+    @size -= 1
+  end
+
 end
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,6 +1,7 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload, :next_item, :last
+  attr_reader :next_item, :last
+  attr_accessor :payload
 
   def initialize(payload)
     @payload = payload
